b1e95b2681e14574aaa7f4882ee2e578428ef4b5
Sokhibdzhon <sokhibsaid@gmail.com>
2020-10-12 23:49:23 +0300

    Revert "Delete ViewModelMapKey.kt"
    This reverts commit d6b3ee6a

---
+++ app/src/main/java/com/sokhibdzhon/livedota/di/viewmodel/ViewModelMapKey.kt
@@ -0,0 +1,23 @@
+package com.sokhibdzhon.livedota.di.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+
+/**     I ❤ Code:)
+╔═══════════════════════════════════════╗
+║  Created by Sokhibdzhon Saidmuratov   ║
+╠═══════════════════════════════════════╣
+║ sokhibsaid@gmail.com                  ║
+╚═══════════════════════════════════════╝
+ */
+
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MapKey
+internal annotation class ViewModelMapKey(val value: KClass<out ViewModel>)
