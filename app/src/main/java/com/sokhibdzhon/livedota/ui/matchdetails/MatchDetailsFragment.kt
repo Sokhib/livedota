fbbd35710c1b3d1f18dc12c39b5be76f3cbd44e1
Sokhibdzhon <sokhibsaid@gmail.com>
2020-10-16 03:30:17 +0300

    [LVD] #refactor #comment MatchDetailsFragment refactored

--- app/src/main/java/com/sokhibdzhon/livedota/ui/matchdetails/MatchDetailsFragment.kt
+++ app/src/main/java/com/sokhibdzhon/livedota/ui/matchdetails/MatchDetailsFragment.kt
@@ -1,15 +1,11 @@
 package com.sokhibdzhon.livedota.ui.matchdetails
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.sokhibdzhon.livedota.R
+import com.sokhibdzhon.livedota.base.BaseFragment
 import com.sokhibdzhon.livedota.data.Status
 import com.sokhibdzhon.livedota.data.model.MatchId
 import com.sokhibdzhon.livedota.databinding.MatchDetailsFragmentBinding
@@ -19,27 +15,16 @@ import com.sokhibdzhon.livedota.util.extensions.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MatchDetailsFragment : Fragment() {
+class MatchDetailsFragment :
+    BaseFragment<MatchDetailsFragmentBinding>(R.layout.match_details_fragment) {
 
     private val viewModel: MatchDetailsViewModel by viewModels()
-    private lateinit var binding: MatchDetailsFragmentBinding
     private var matchId: MatchId = MatchId(0)
     private var leagueName: String = "League Name"
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding =
-            DataBindingUtil.inflate(inflater, R.layout.match_details_fragment, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-
-        return binding.root
-    }
-
     private val args: MatchDetailsFragmentArgs by navArgs()
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+    override fun init() {
         matchId = MatchId(args.matchId)
         leagueName = args.leagueName
     }
@@ -55,48 +40,23 @@ class MatchDetailsFragment : Fragment() {
 //        }
 
         //Get Match Details
-        viewModel.matchDetailsLiveData.observe(viewLifecycleOwner, Observer { matchDetails ->
-            when (matchDetails.matchDetailsResource.status) {
-                Status.SUCCESS -> {
-                    binding.viewState = matchDetails
-                }
-                Status.ERROR -> {
-                    toast(requireActivity(), getString(R.string.error_fetching))
-                }
-            }
-        })
-        //Get Radiant Team Logo Id
-        viewModel.radiantTeamLogoId.observe(viewLifecycleOwner, Observer { radiantLogoId ->
-            radiantLogoId.let { viewModel.getTeamLogo(radiantLogoId, Teams.RADIANT) }
-        })
-        //Get Dire Team Logo Id
-        viewModel.direTeamLogoId.observe(viewLifecycleOwner, Observer { direLogoId ->
-            direLogoId.let { viewModel.getTeamLogo(direLogoId, Teams.DIRE) }
-        })
+        getMatchDetails()
         //Get Radiant Team Logo
-        viewModel.radiantTeamLogo.observe(viewLifecycleOwner, Observer {
-            when (it.teamLogo.status) {
-                Status.SUCCESS -> {
-                    binding.radiantState = it
-                }
-                Status.ERROR -> {
-                    toast(requireActivity(), getString(R.string.no_radiant_logo))
-                }
-            }
-        })
+        getRadiantTeamLogo()
         //Get Dire Team Logo
-        viewModel.direTeamLogo.observe(viewLifecycleOwner, Observer {
-            when (it.teamLogo.status) {
-                Status.SUCCESS -> {
-                    binding.direState = it
-                }
-                Status.ERROR -> {
-                    toast(requireActivity(), getString(R.string.no_dire_logo))
-                }
-            }
-        })
+        getDireTeamLogo()
+        //Get Players
+        getPlayers()
+        //Set League Name
+        setLeagueName()
 
-        //Get Players Id
+    }
+
+    private fun setLeagueName() {
+        binding.leagueName.text = leagueName
+    }
+
+    private fun getPlayers() {
         viewModel.playerIds.observe(viewLifecycleOwner, Observer { playerIdList ->
             playerIdList.let { viewModel.getPlayers(playerIdList) }
         })
@@ -107,8 +67,52 @@ class MatchDetailsFragment : Fragment() {
                 10 -> binding.playersState = PlayerViewState(playersList)
             }
         })
+    }
 
-        binding.leagueName.text = leagueName
+    private fun getDireTeamLogo() {
+        viewModel.direTeamLogoId.observe(viewLifecycleOwner, Observer { direLogoId ->
+            direLogoId.let { viewModel.getTeamLogo(direLogoId, Teams.DIRE) }
+        })
+        viewModel.direTeamLogo.observe(viewLifecycleOwner, Observer {
+            when (it.teamLogo.status) {
+                Status.SUCCESS -> {
+                    binding.direState = it
+                }
+                Status.ERROR -> {
+                    toast(requireActivity(), getString(R.string.no_dire_logo))
+                }
+            }
+        })
+    }
+
+    private fun getRadiantTeamLogo() {
+        viewModel.radiantTeamLogoId.observe(viewLifecycleOwner, Observer { radiantLogoId ->
+            radiantLogoId.let { viewModel.getTeamLogo(radiantLogoId, Teams.RADIANT) }
+        })
+
+        viewModel.radiantTeamLogo.observe(viewLifecycleOwner, Observer {
+            when (it.teamLogo.status) {
+                Status.SUCCESS -> {
+                    binding.radiantState = it
+                }
+                Status.ERROR -> {
+                    toast(requireActivity(), getString(R.string.no_radiant_logo))
+                }
+            }
+        })
+    }
+
+    private fun getMatchDetails() {
+        viewModel.matchDetailsLiveData.observe(viewLifecycleOwner, Observer { matchDetails ->
+            when (matchDetails.matchDetailsResource.status) {
+                Status.SUCCESS -> {
+                    binding.viewState = matchDetails
+                }
+                Status.ERROR -> {
+                    toast(requireActivity(), getString(R.string.error_fetching))
+                }
+            }
+        })
     }
 
 
